e3afe193638854486a1d08f178da9da0cdb2a39b
Greens-StokesTheorem <jessicaleal2000why@gmail.com>
2025-06-07 20:18:36 +0100

    upload

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,16 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({}) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
+export function loader({ context }) {
   return { message: context.VALUE_FROM_EXPRESS };
 }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function Home({ loaderData }: Route) {
   return <Welcome message={loaderData.message} />;
 }
